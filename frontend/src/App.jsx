90e0fe8f69a13d2f321ec172d8713798b970a6f1
Artyom Sapa <artem.sapa@gmail.com>
2024-07-02 19:12:37 -0700

    count as first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import AnalysisView from './pages/AnalysisView';
+import UserView from './pages/UserView';
 
 const App = () => {
   return (
